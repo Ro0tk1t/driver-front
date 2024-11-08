5ce2c3cb209dfbe2c6090b73a8870f527f8ef4df
r00tk1t <r00tk1t@qq.com>
2024-11-08 16:33:20 +0800

    use dall e 3

--- src/components/index.vue
+++ src/components/index.vue
@@ -3,24 +3,29 @@ import { StarFilled } from '@element-plus/icons-vue'
 </script>
 
 <template>
-  <el-carousel :interval="3000" type="card" height="400px">
-    <el-carousel-item v-for="item in 5" :key="item">
-      <h3>{{ item }}</h3>
+  <el-carousel :interval="3000" type="card" height="354px">
+    <el-carousel-item v-for="item in 3" :key="item">
+      <!-- <h5>{{ item }}</h5> -->
+       <!--快  安全  简单-->
+      <el-image :src="`feat${item}.webp`"></el-image>
     </el-carousel-item>
   </el-carousel>
 
   <div>
     <span>一款在线网盘. </span>
-    <el-divider content-position="left">Rabindranath Tagore</el-divider>
+    <el-divider content-position="left">>>>>>>>>>>>>>>></el-divider>
     <span>
-      My wishes are fools, they shout across thy song, my Master. Let me but
-      listen.
+       快速         安全          简单易用
     </span>
     <el-divider>
       <el-icon><star-filled /></el-icon>
+      <el-icon><star-filled /></el-icon>
+      <el-icon><star-filled /></el-icon>
+      <el-icon><star-filled /></el-icon>
+      <el-icon><star-filled /></el-icon>
     </el-divider>
-    <span>快来下载使用吧 .</span>
-    <el-divider content-position="right">Rabindranath Tagore</el-divider>
+    <span>快来使用吧 .</span>
+    <el-divider content-position="right"><<<<<<<<<<<<<<<<</el-divider>
   </div>
 </template>
 
